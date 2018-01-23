8e042c1b95d67c776ddd78907e126ec3b2d550b3
Oliver Hall <ojh251@student.bham.ac.uk>
2018-01-23 11:30:57 +0000

    Fixed bugs in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from setuptools import setup
 setup(name='ClosePlots',
       version='0.1',
       description='A button that lets you close subplots.',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
-      license='MIT',
-      packages=['funniest'],
+      url='https://github.com/ojhall94/ClosePlots',
+      author='Oliver James Hall',
+      author_email='ojh251@student.bham.ac.uk',
+      license='University of Birmingham',
+      packages=['ClosePlots'],
+      install_requires=[
+            'matplotlib',
+      ],
       zip_safe=False)
